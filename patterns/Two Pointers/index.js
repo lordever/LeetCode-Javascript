0757cfe1678745ffa886af904013018c83462a4c
lordever <lordever@mail.ru>
2024-01-24 21:59:30 +0600

    Remove Duplicates from Sorted Array was completed

--- patterns/Two Pointers/index.js
+++ patterns/Two Pointers/index.js
@@ -35,3 +35,21 @@ console.log(twoPointers([2, 3, 4, 5, 9], 1)) //[]
 console.log(twoPointers([1, 2, 4, 6, 7], 8)) //[0, 4]
 console.log(twoPointers([], 8)) //[]
 
+//Array
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
+
+    let uniqueIndex = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[uniqueIndex] = nums[i];
+            uniqueIndex++;
+        }
+    }
+
+    return uniqueIndex;
+};
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(nums)); //4
+
